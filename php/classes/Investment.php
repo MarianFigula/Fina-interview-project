8acc25cb2c0e695188afee06b573b8eb909b0dfd
Marian Figula <majofigulam3@gmail.com>
2024-09-18 14:27:15 +0200

    init: project initialization

---
+++ php/classes/Investment.php
@@ -0,0 +1,61 @@
+<?php
+
+class Investment
+{
+    private $id;
+    private string $title;
+    private $value;
+    private $percentage;
+    private $date_of_creation;
+
+    // Constructor
+    public function __construct($id, $title, $value, $percentage, $date_of_creation) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->value = $value;
+        $this->percentage = $percentage;
+        $this->date_of_creation = $date_of_creation;
+    }
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getValue() {
+        return $this->value;
+    }
+
+    public function getPercentage() {
+        return $this->percentage;
+    }
+
+    public function getDateOfCreation() {
+        return $this->date_of_creation;
+    }
+
+    // Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function setValue($value) {
+        $this->value = $value;
+    }
+
+    public function setPercentage($percentage) {
+        $this->percentage = $percentage;
+    }
+
+    public function setDateOfCreation($date_of_creation) {
+        $this->date_of_creation = $date_of_creation;
+    }
+}
