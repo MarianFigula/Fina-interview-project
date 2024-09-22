2cb852e5796e6d34c928a39e1051208f89aec76c
Marian Figula <majofigulam3@gmail.com>
2024-09-22 21:28:07 +0200

    fix: bug fixes

--- js/add-investment-modal.js
+++ js/add-investment-modal.js
@@ -3,10 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const openDialogButton = document.getElementById('invest-btn');
     const closeDialogButton = document.getElementById('closeDialog');
     const investmentForm = document.getElementById('addInvestmentForm');
+    const submitButton = document.querySelector('dialog button[type="submit"]')
     const totalPortfolio = updateTotalInvestment()
 
-
     openDialogButton.onclick = function() {
+        submitButton.classList.replace('btn-edit', 'btn-proceed')
         dialog.showModal();
     }
 
@@ -26,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const investmentData = {
             title: title,
             value: price,
-            percentage: (price / totalPortfolio) * 100,
+            percentage: ((price / totalPortfolio) * 100).toFixed(2),
             type: type
         };
 
