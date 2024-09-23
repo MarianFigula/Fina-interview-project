7643558e71e29a8aca81a52e0e925220a000355c
Marian Figula <majofigulam3@gmail.com>
2024-09-23 11:57:02 +0200

    feat: making a request on javascript to create an investment

--- js/add-investment-modal.js
+++ js/add-investment-modal.js
@@ -35,12 +35,43 @@ document.addEventListener('DOMContentLoaded', () => {
             type: type
         };
 
-        const existingInvestments = JSON.parse(localStorage.getItem('investments')) || [];
-        existingInvestments.push(investmentData);
-        localStorage.setItem('investments', JSON.stringify(existingInvestments));
+        await createInvestment(investmentData)
 
         dialog.close();
         investmentForm.reset();
         await updateTotalInvestment();
     }
+
+
+    async function createInvestment(investmentData) {
+        try {
+            const response = await fetch('/php/api/investment/create.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(investmentData)
+            });
+
+            if (!response.ok) {
+                console.log("Server side error")
+                return
+            }
+
+            const result = await response.json();
+
+            if (result.success) {
+                alert('Investment added successfully!');
+                localStorage.setItem('investments', JSON.stringify(result.investments));
+                location.reload();
+            } else {
+                alert('Error adding investment: ' + result.message);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('There was an error adding the investment. Please try again.');
+        }
+    }
 });
+
+
