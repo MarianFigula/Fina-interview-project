b1a8d6af373745ec0260295185d6cc63a560475d
Marian Figula <majofigulam3@gmail.com>
2024-09-23 10:01:32 +0200

    fix: checking if something is in local storage and if yes, return it right there

--- js/read-investments.js
+++ js/read-investments.js
@@ -1,18 +1,21 @@
 const getInvestments = async () => {
     let investmentData = JSON.parse(localStorage.getItem('investments'));
 
-    if (!investmentData || investmentData.length === 0) {
-        try {
-            const response = await fetch('/api/investment/read.php');
-
-            if (!response.ok) console.log("Server side error")
-
-            investmentData = await response.json();
-            localStorage.setItem('investments', JSON.stringify(investmentData));
-        } catch (error) {
-            console.error('Error fetching investments:', error);
-        }
+    if (investmentData && investmentData.length > 0){
+        return investmentData
     }
+
+    try {
+        const response = await fetch('/api/investment/read.php');
+
+        if (!response.ok) console.log("Server side error")
+
+        investmentData = await response.json();
+        localStorage.setItem('investments', JSON.stringify(investmentData));
+    } catch (error) {
+        console.error('Error fetching investments:', error);
+    }
+
 
     return investmentData;
 };
