725286fa562b4844923d0dbe7e66ae719928b7be
Marian Figula <majofigulam3@gmail.com>
2024-09-23 09:51:18 +0200

    feat: check if !response.ok

--- js/read-investments.js
+++ js/read-investments.js
@@ -4,6 +4,9 @@ const getInvestments = async () => {
     if (!investmentData || investmentData.length === 0) {
         try {
             const response = await fetch('/api/read.php');
+
+            if (!response.ok) console.log("Server side error")
+
             investmentData = await response.json();
             localStorage.setItem('investments', JSON.stringify(investmentData));
         } catch (error) {
