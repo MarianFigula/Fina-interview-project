120f769520e345c8330279cebb52e3314e1b51b8
Marian Figula <majofigulam3@gmail.com>
2024-09-23 10:45:10 +0200

    feat: read.php will now get the investments from json

--- php/api/investment/read.php
+++ php/api/investment/read.php
@@ -1,10 +1,15 @@
 <?php
 
-require_once 'php/classes/Investment.php';
+require_once '../../classes/Investment.php';
 header('Content-Type: application/json');
 
-$investment = new Investment();
+try {
+    $investmentObj = new Investment();
 
-$investments = $investment->getInvestments();
-
-echo json_encode($investments);
+    $investments = $investmentObj->getInvestments();
+    http_response_code(200);
+    echo json_encode($investments);
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(['error' => $e->getMessage()]);
+}
